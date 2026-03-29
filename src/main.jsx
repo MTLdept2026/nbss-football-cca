44b6bf4c9c759a249612878b41b3c322d2209211
MTLdept2026 <antimagik@gmail.com>
2026-03-29 19:02:49 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import AccessGate from './components/AccessGate'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AccessGate>
+      <App />
+    </AccessGate>
+  </React.StrictMode>
+)
